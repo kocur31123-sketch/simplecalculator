cddfc5b64d97a8077ae18c2e0f19d70291b5d5c8
kocur31123-sketch <kocur31123@wp.pl>
2026-07-03 17:58:52 +0200

    Add project.files

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplecalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+       
+            
+            {
+                Console.Write("Podaj pierwszą liczbę: ");
+                double a = double.Parse(Console.ReadLine());
+
+                Console.Write("Podaj drugą liczbę: ");
+                double b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Wybierz działanie (+, -, *, /): ");
+                char op = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                double wynik;
+
+                switch (op)
+                {
+                    case '+':
+                        wynik = a + b;
+                        break;
+                    case '-':
+                        wynik = a - b;
+                        break;
+                    case '*':
+                        wynik = a * b;
+                        break;
+                    case '/':
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Nie można dzielić przez zero.");
+                            return;
+                        }
+                        wynik = a / b;
+                        break;
+                    default:
+                        Console.WriteLine("Nieznane działanie.");
+                        return;
+                }
+
+                Console.WriteLine($"Wynik: {wynik}");
+            }
+        }
+
+    }
+}
+
